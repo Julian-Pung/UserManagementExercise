b20d7f9953a7167cc360b3042351921764cbd7d0
Julian Pung <julian.pung@web.de>
2020-03-07 03:11:07 +0100

    Vuetify mit Basislayout hinzufügen

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,47 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+	<v-app>
+		<v-app-bar app color="primary" dark clipped-left>
+			<v-toolbar-title>User Management</v-toolbar-title>
+			<v-spacer></v-spacer>
+		</v-app-bar>
+
+		<v-navigation-drawer app clipped>
+			<v-list dense>
+				<v-list-item v-for="item in menuItems" :key="item.title" link>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>
+						<v-list-item-title>{{ item.title }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-content class="grey lighten-3">
+			<router-view></router-view>
+		</v-content>
+	</v-app>
 </template>
 
-<style>
-</style>
+<script>
+	export default {
+		name: 'App',
+		data: () => ({
+			menuItems: [
+				{
+					icon: 'mdi-account',
+					title: 'Benutzer'
+				},
+				{
+					icon: 'mdi-account-box-outline',
+					title: 'Rollen'
+				},
+				{
+					icon: 'mdi-security',
+					title: 'Berechtigungen'
+				}
+			]
+		})
+	};
+</script>
